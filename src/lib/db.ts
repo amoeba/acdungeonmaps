82f68d477da842c1b81e8fa9b99c515cc1325843
Bryce Mecum <petridish@gmail.com>
2021-11-21 20:53:53 -0900

    Fix missing rotation on tiles query

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,7 @@ const DUNGEONS_TBL_NAME = "dungeons";
 export const DB_HOST = import.meta.env.DB_URI || `http://localhost:8001`
 
 const TILES_QUERY = function (id: string): string {
-  return `select+x%2C+y%2C+z%2C+environment_id+%2Cname+from+${TILES_TBL_NAME}+left+join+${DUNGEONS_TBL_NAME}+on+${DUNGEONS_TBL_NAME}.landblock_id+%3D%3D+${TILES_TBL_NAME}.landblock_id+where+${TILES_TBL_NAME}.landblock_id+%3D+%27${id}%27+%3B`;
+  return `select+x%2C+y%2C+z%2C+rotation%2C+environment_id+%2Cname+from+${TILES_TBL_NAME}+left+join+${DUNGEONS_TBL_NAME}+on+${DUNGEONS_TBL_NAME}.landblock_id+%3D%3D+${TILES_TBL_NAME}.landblock_id+where+${TILES_TBL_NAME}.landblock_id+%3D+%27${id}%27+%3B`;
 }
 
 export const TILE_URL = function (id: string): string {
