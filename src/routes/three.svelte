fb1d6a555e66fb41d7d7d5d051d072b0c8e17db8
Bryce Mecum <petridish@gmail.com>
2023-02-03 12:43:28 -0900

    First real working version of 3D

--- src/routes/three.svelte
+++ src/routes/three.svelte
@@ -7,55 +7,69 @@
     BoxGeometry,
     MeshBasicMaterial,
     Mesh,
+    TextureLoader,
+    Vector2
   } from "three";
   import {OrbitControls} from "../lib/OrbitControls"
 
   let el: Element;
+  const tl  = new TextureLoader();
 
-  const addDummyCubes = function (scene) {
-    const material = new MeshBasicMaterial({
-      color: 0x3333dd,
-    });
-
-    for (var i = 0; i < 100; i++) {
-      let geometry = new BoxGeometry(1, 1, 0.1);
-      let cube = new Mesh(geometry, material);
-      cube.position.x = (i % 10) * 2;
-      cube.position.y = Math.floor(i / 10) * 2;
-
-      scene.add(cube);
+  const getRotation = function(w : number): number {
+    if (w == 1) {
+      return 0;
+    } else if (w < -0.70 && w > -0.8) {
+      return 90;
+    } else if (w > 0.70 && w < 0.8) {
+      return -90;
+    } else {
+      return 180;
     }
-  };
+  }
 
   const addTiles = function (scene, tiles) {
-    const material = new MeshBasicMaterial({
-      color: 0x3333dd,
-      opacity: 0.5,
-    });
-    material.transparent = true;
-
     for (var i = 0; i < tiles.length; i++) {
+      const w = tiles[i][3];
+      // Note: Extra +180 here was needed to get tiles right
+      const rot = getRotation(w) + 180;
+
+      // Texture
+      let texture = tl.load(`/tiles/${tiles[i][4]}.bmp`);
+
+      // Important to center, I think?
+      texture.center = new Vector2(0.5, 0.5);
+      texture.rotation = rot * (Math.PI / 180);
+
+      // Material
+      let material = new MeshBasicMaterial({ map: texture });
+
+      // Geometry and Mesh
       let geometry = new BoxGeometry(10, 1, 10);
       let cube = new Mesh(geometry, material);
+
       cube.position.x = tiles[i][0];
       cube.position.y = tiles[i][2];
       cube.position.z = tiles[i][1];
 
+      // I needed this to get tiles to look right
+      cube.scale.x = -1;
+
       scene.add(cube);
     }
+
   };
 
   onMount(async () => {
     // Fetch
-    const id = "011B";
+    const id = "0012";
     const url = `https://dungeonmapsdb.treestats.net/dungeonmaps.json?sql=select+x%2C+y%2C+z%2C+rotation%2C+environment_id+%2Cname+from+tiles+left+join+dungeons+on+dungeons.landblock_id+%3D%3D+tiles.landblock_id+where+tiles.landblock_id+%3D+%27${id}%27+%3B&_size=max`;
 		const response = await fetch(url);
     const json = await response.json();
     const tiles = json["rows"];
 
     // Scene
-    const width = 1024;
-    const height = 768;
+    const width = 600;
+    const height = 600;
 
     const scene = new Scene();
     const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
@@ -67,10 +81,9 @@
 
     // Controls
     const controls = new OrbitControls( camera, renderer.domElement );
-    camera.position.set( 0, 20, 100 );
+    camera.position.set( 0, 100, 0 );
     controls.update();
 
-    // addDummyCubes(scene);
     addTiles(scene, tiles);
 
 
