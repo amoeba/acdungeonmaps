3c8a192c51ebf5b4e83681821af17046ead34789
Bryce Mecum <petridish@gmail.com>
2023-02-06 22:53:11 -0900

    Set landblock name in dungeon map vie

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,20 +1,27 @@
-
 const DB_NAME = "dungeonmaps";
 const TILES_TBL_NAME = "tiles";
 const DUNGEONS_TBL_NAME = "dungeons";
 
-export const DB_HOST = import.meta.env.VITE_DB_URI || `http://localhost:8001`
+export const DB_HOST = import.meta.env.VITE_DB_URI || `http://127.0.01:8001`;
+
+const LANDBLOCK_NAME_QUERY = function (id: string): string {
+  return `SELECT%20name%20FROM%20dungeons%20WHERE%20landblock_id%20=%20%220001%22%20LIMIT%201;`;
+};
+
+export const LANDBLOCK_NAME_URL = function (id: string): string {
+  return `${DB_HOST}/${DB_NAME}.json?sql=${LANDBLOCK_NAME_QUERY(id)}`;
+};
 
 const TILES_QUERY = function (id: string): string {
   return `select+x%2C+y%2C+z%2C+rotation%2C+environment_id+%2Cname+from+${TILES_TBL_NAME}+left+join+${DUNGEONS_TBL_NAME}+on+${DUNGEONS_TBL_NAME}.landblock_id+%3D%3D+${TILES_TBL_NAME}.landblock_id+where+${TILES_TBL_NAME}.landblock_id+%3D+%27${id}%27+%3B`;
-}
+};
 
 export const TILE_URL = function (id: string): string {
   return `${DB_HOST}/${DB_NAME}.csv?sql=${TILES_QUERY(id)}&_size=max`;
-}
+};
 
 export const TILE_URL_JSON = function (id: string): string {
   return `${DB_HOST}/${DB_NAME}.json?sql=${TILES_QUERY(id)}&_size=max`;
-}
+};
 
-export const LANDBLOCKS_URL = `${DB_HOST}/${DB_NAME}.json?sql=select%20*%20from%20${DUNGEONS_TBL_NAME}%20limit%201000;`
+export const LANDBLOCKS_URL = `${DB_HOST}/${DB_NAME}.json?sql=select%20*%20from%20${DUNGEONS_TBL_NAME}%20limit%201000;`;
--- src/routes/dungeons/[id].svelte
+++ src/routes/dungeons/[id].svelte
@@ -1,11 +1,15 @@
 <script context="module" lang="ts">
-  /**
-   * @type {import('@sveltejs/kit').Load}
-   */
-  export async function load({ params, fetch, session, stuff }) {
+  import { LANDBLOCK_NAME_URL } from "../../lib/db"
+
+  /** @type {import('./$types').PageLoad} */
+  export async function load({ fetch, params }) {
+    const res = await fetch(LANDBLOCK_NAME_URL(params.id));
+    const data = await res.json();
+
     return {
       props: {
         id: params.id,
+        name: data["rows"][0][0]
       },
     };
   }
@@ -15,10 +19,26 @@
   import TabbedMap from "../../components/TabbedMap.svelte";
 
   export let id;
+  export let name;
 </script>
 
 <svelte:head>
-  <title>{id}</title>
+  <title>{name} (0x{id})</title>
 </svelte:head>
 
+<h2>Viewing: <a href={id}>{name} (0x{id})</a></h2>
 <TabbedMap id={id} />
+
+<style>
+  h2 {
+    font-size: 115%;
+    margin: 0;
+  }
+  h2 a {
+    text-decoration: none;
+  }
+
+  h2 a:hover {
+    text-decoration: underline;
+  }
+</style>
