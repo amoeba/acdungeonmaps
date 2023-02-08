13e9a180e3e5be1e9fa60e567840b90d63f97b00
Bryce Mecum <petridish@gmail.com>
2023-02-07 21:35:00 -0900

    Oof

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
-import { VERCEL_COMMIT_REF } from "$env/static/private";
+import { COMMIT_REF } from "$env/static/private";
 
 /** @type {import('./$types').LayoutServerLoad} */
 export function load() {
   return {
-    COMMIT_REF: VERCEL_COMMIT_REF,
+    COMMIT_REF: COMMIT_REF,
   };
 }
