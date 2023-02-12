48ee9a43adf9ffa8b2ed57aacdd816611de2b06c
Bryce Mecum <petridish@gmail.com>
2023-02-11 17:20:57 -0900

    Handle dungeon lookup 404

---
+++ src/routes/dungeons/[id]/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h2>{$page.status}</h2>
+<p>{$page.error.message}</p>
+
+<style>
+  h2,
+  p {
+    margin: 0;
+    padding: 0;
+  }
+</style>
--- src/routes/dungeons/[id]/+page.ts
+++ src/routes/dungeons/[id]/+page.ts
@@ -1,9 +1,17 @@
+import { error } from "@sveltejs/kit";
 import { LANDBLOCK_NAME_URL } from "../../../lib/db";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
   const res = await fetch(LANDBLOCK_NAME_URL(params.id));
   const data = await res.json();
+
+  // Handle tiles not found with a 404
+  if (!data || data["rows"] || data["rows"] <= 0) {
+    throw error(404, {
+      message: `No tiles found for dungeon with id 0x${params.id}`,
+    });
+  }
 
   return {
     id: params.id,
