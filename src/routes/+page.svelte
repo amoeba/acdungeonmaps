cf1da4ee09bceeae9220936365926a4aa81a070e
Bryce Mecum <petridish@gmail.com>
2023-05-08 20:51:24 -0800

    Add note about new UB maps site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
   import LandblockList from "../components/LandblockList.svelte";
 </script>
 
+<p>
+  <em
+    >Note (2023-05-08): You might also check out trevis' more advanced <a
+      href="https://maps.utilitybelt.me/">Utility Belt Maps</a
+    > project.</em
+  >
+</p>
+
 <LandblockList />
